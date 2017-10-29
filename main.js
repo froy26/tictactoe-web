dad264e4898c048ea565ad00959575223a468621
Felix Roy <felix.roy.26@gmail.com>
2017-10-28 23:40:37 -0400

    Working on base code.

---
+++ main.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+
+//Declare public resources.
+app.use(express.static("public"));
+
+app.get('/', function(req, res){
+    var indexFilePath = path.join(__dirname, "views", "index.html");
+    
+    res.sendFile(indexFilePath);
+});
+
+app.listen(3000);

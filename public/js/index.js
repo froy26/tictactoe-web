642d71e899d91310c689f6ab8b76d8a20e2ef86c
Felix Roy <felix.roy.26@gmail.com>
2017-11-18 16:39:40 -0500

    Added solution list.

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,50 @@
 console.log("OK");
 
 var tiles = document.getElementsByClassName("tile");
-
+var solutions = [
+	[ //Solution for tile 0.
+		[1,2], //Horizontal
+		[4,8], //Diagonal
+		[3,6]  //Vertical
+	],
+	[ //Solution for tile 1.
+		[0,2], //Horizontal
+		[4,7]  //Vertical
+	],
+	[ //Solution for tile 2.
+		[0,1], //Horizontal
+		[4,6], //Diagonal
+		[5,8]  //Vertical
+	],
+	[ //Solution for tile 3.
+		[4,5], //Horizontal
+		[0,6]  //Vertical
+	],
+	[ //Solution for tile 4.
+		[3,5], //Horizontal
+		[2,6], //Diagonal
+		[0,8], //Diagonal
+		[1,7]  //Vertical
+	],
+	[ //Solution for tile 5.
+		[4,3], //Horizontal
+		[2,8]  //Vertical
+	],
+	[ //Solution for tile 6.
+		[7,8], //Horizontal
+		[2,4], //Diagonal
+		[0,3]  //Vertical
+	],
+	[ //Solution for tile 7.
+		[6,8], //Horizontal
+		[1,4]  //Vertical
+	],
+	[ //Solution for tile 8.
+		[6,7], //Horizontal
+		[0,4], //Diagonal
+		[2,5]  //Vertical
+	],
+];
 
 window.addEventListener('load', function(){
 	initTiles();

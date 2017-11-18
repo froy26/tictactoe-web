80ec95b334e2be6b523f260ef6b38b79148120ef
Felix Roy <felix.roy.26@gmail.com>
2017-11-18 16:04:56 -0500

    Juju est mad

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,23 @@
 console.log("OK");
 
-var cases = document.getElementsByClassName("tile");
+var tiles = document.getElementsByClassName("tile");
+
+
+window.addEventListener('load', function(){
+    for(var i = 0; i<tiles.length;i++)
+    {
+        var tile = tiles[i];
+        console.log(i);
+
+        tile.addEventListener('click', function(event){
+            if((!event.target.classList.contains("x") && (!event.target.classList.contains("o"))))
+            {
+                event.target.classList.add("active","x");
+            }
+        });
+        
+    }
+
+
+
+});

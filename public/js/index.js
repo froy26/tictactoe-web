0f5fbfbfeb4f18f28e41c6a0b0b9efaf95670da3
IMFUZZ <da.junior.du@gmail.com>
2017-11-18 16:07:36 -0500

    spaces to tabs, removed trailing whitespaces

--- public/js/index.js
+++ public/js/index.js
@@ -4,20 +4,16 @@ var tiles = document.getElementsByClassName("tile");
 
 
 window.addEventListener('load', function(){
-    for(var i = 0; i<tiles.length;i++)
-    {
-        var tile = tiles[i];
-        console.log(i);
+	for(var i = 0; i<tiles.length;i++)
+	{
+		var tile = tiles[i];
+		console.log(i);
 
-        tile.addEventListener('click', function(event){
-            if((!event.target.classList.contains("x") && (!event.target.classList.contains("o"))))
-            {
-                event.target.classList.add("active","x");
-            }
-        });
-        
-    }
-
-
-
+		tile.addEventListener('click', function(event){
+			if((!event.target.classList.contains("x") && (!event.target.classList.contains("o"))))
+			{
+				event.target.classList.add("active","x");
+			}
+		});
+	}
 });
